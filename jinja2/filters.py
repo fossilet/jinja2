caa77e8d8bb93efb78db020845ade251b54cf4cc
Yongzhi Pan <panyongzhi@gmail.com>
2014-04-04 17:17:17 +0800

    Fix wordwrap bug.
    https://github.com/mitsuhiko/jinja2/issues/175

--- jinja2/filters.py
+++ jinja2/filters.py
@@ -483,19 +483,22 @@ def do_wordwrap(environment, s, width=79, break_long_words=True,
     Return a copy of the string passed to the filter wrapped after
     ``79`` characters.  You can override this default using the first
     parameter.  If you set the second parameter to `false` Jinja will not
-    split words apart if they are longer than `width`. By default, the newlines
-    will be the default newlines for the environment, but this can be changed
-    using the wrapstring keyword argument.
-
-    .. versionadded:: 2.7
-       Added support for the `wrapstring` parameter.
+    split words apart if they are longer than `width`.
     """
-    if not wrapstring:
-        wrapstring = environment.newline_sequence
     import textwrap
-    return wrapstring.join(textwrap.wrap(s, width=width, expand_tabs=False,
-                                   replace_whitespace=False,
-                                   break_long_words=break_long_words))
+    accumulator = []
+    # Workaround: pre-split the string
+    for component in re.split(r"\r?\n", s):
+        # textwrap will eat empty strings for breakfirst. Therefore we route them around it.
+        if len(component) is 0:
+            accumulator.append(component)
+            continue
+        accumulator.extend(
+            textwrap.wrap(component, width=width, expand_tabs=False,
+                replace_whitespace=False,
+                break_long_words=break_long_words)
+        )
+    return environment.newline_sequence.join(accumulator)
 
 
 def do_wordcount(s):
